15f3c9d15f577dcd612726c639bb0db01b67e2ef
shdwmsk <2968159181905@ingenieria.usac.edu.gt>
2025-02-28 19:30:37 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
+
+// Routes
+
+// Server
+class Server {
+    constructor() {
+        
+    }
+}
